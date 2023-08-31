93171ae9415a7f94778f29716289b769bdde0eb6
HaNeul <moho009804@gmail.com>
2023-08-31 17:10:17 +0900

    Update index.tsx

--- Front/sulnaeeum/src/pages/index.tsx
+++ Front/sulnaeeum/src/pages/index.tsx
@@ -53,14 +53,8 @@ export default function Home() {
         <title>SULNAEEUM | 술내음</title>
       </Head>
       <div className="h-[6400px] overflow-hidden scroll-smooth">
-        {/* <div className="text-red-600 fixed z-30 right-1/2 top-1/2">
-          <p>Scroll Value : {scroll}</p>
-          <p>translateY :{translateY}</p>
-          <p>image : {imageX}</p>
-        </div> */}
-
+        
         {/* PART 1 */}
-        {/*흰 텍스트1 */}
         <div
           className={` ${styles.scrolled} w-[4500px] ${
             isScrolled ? "absolute z-30" : "fixed"
@@ -77,7 +71,6 @@ export default function Home() {
             height={1000}
           />
         </div>
-        {/*전통주 배경화면 */}
         <div
           className={` w-[4000px] h-[300px] ${
             isScrolled ? " absolute z-10 top-[1230px]" : "fixed"
@@ -94,15 +87,6 @@ export default function Home() {
             <source src="/main/part1/배경화면_최종.mp4" type="video/mp4" />
           </video>
 
-          {/* <img className=" z-20 absolute top-[0px] w-[1920px] h-[1080px]" src="/main/part1/메인페이지_배경.png" alt=""></img> */}
-
-          {/* <Image
-            className={`z-20 absolute top-[-150px]`}
-            src="/main/part1/note18_RE.jpg"
-            alt=""
-            width={1900}
-            height={0}
-          /> */}
           <div className=" absolute z-40 top-[650px] left-[-110px] ">
             <Image
               src="/main/part1/main1_txt2.png"
@@ -120,16 +104,14 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/* 한국 전통주 정의 */}
+
         <div className="absolute left-[220px] top-[2500px]">
           <Image src="/main/part2/main_2.png" alt="" width={1500} height={0} />
         </div>
 
         {/* PART 2 */}
         <div className="absolute  top-[2350px] w-screen h-[1000px]  flex">
-          {/* 미리보기 */}
           <div className="absolute">
-            {/* <Image src="/main/part2/main2_bg.png" alt="" width={1910} height={100}></Image> */}
           </div>
           <div className="absolute  top-[305px] left-[430px] w-[700px] h-[400px]">
             <div className="absolute text-4xl font-preB text-[#977857]">
@@ -172,27 +154,24 @@ export default function Home() {
               height={0}
             />
           </div>
+          
           {/* 스크롤 애니메이션 */}
           <div className="absolute top-[200px] w-screen h-[300px] ">
             <Image
-              className="
-               
-              absolute z-20 ml-[-10px]"
+              className="absolute z-20 ml-[-10px]"
               src="/main/part2/main2_1.png"
               alt=""
               width={400}
               height={0}
             ></Image>
             <Image
-              className="
-            
-               absolute z-10 right-0 mt-[-100px]"
+              className="absolute z-10 right-0 mt-[-100px]"
               src="/main/part2/main2_2.png"
               alt=""
               width={750}
               height={0}
             ></Image>
-            <div className="">
+            <div>
               <Image
                 className={`${
                   scroll > 2100 ? styles.opacity4 : styles.hide
@@ -255,7 +234,7 @@ export default function Home() {
           <div
             className={`${
               scroll > 4000 ? styles.show2 : styles.hide
-            } absolute top-[100px] w-[600px] h-[620px] left-[320px]  `}
+            } absolute top-[100px] w-[600px] h-[620px] left-[320px]`}
           >
             <Image
               className="absolute top-[50px] left-[60px]"
@@ -286,12 +265,7 @@ export default function Home() {
               height={300}
             ></Image>
           </div>
-          {/* <Image
-            src="/main/part4/part4.png"
-            alt=""
-            width={1920}
-            height={200}
-          ></Image> */}
+          
           <div
             className={` ${
               scroll > 3700 ? styles.opacity : styles.hide
@@ -318,8 +292,6 @@ export default function Home() {
 
         {/* PART 5 */}
         <div className="absolute  top-[5500px] w-screen h-[1000px]  flex ">
-          {/* <Image className="opacity-30" src="/main/part5/main5_sample.PNG" alt="" width={1900} height={300}></Image> */}
-          {/* <Image src="/main/part3/main3_bg.png" alt="" width={1800} height={200}></Image> */}
           <div
             className={`${
               scroll > 4600 ? styles.opacity : styles.hide
@@ -345,6 +317,7 @@ export default function Home() {
               </p>
             </div>
           </div>
+          
           {/* 스크롤 애니메이션 */}
           <div className="absolute left-[790px] top-[350px] w-[1000px] h-[410px]">
             <Image
